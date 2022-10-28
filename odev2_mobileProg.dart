7186b13d00000fc57655e3f499f73dfa45fd0f59
OUSSAMA <OUSSAMA@DESKTOP-JVO85LD>
2022-10-28 15:13:45 +0300

    kod uzerinde birkac degisiklik yapildi

--- odev2_mobileProg.dart
+++ odev2_mobileProg.dart
@@ -9,30 +9,42 @@ void main() {
   }
 
   while (list.contains("*") || list.contains("/")) {
-    for (var i = 0; i < list.length; i++) {
+    var i = 0;
+    while (i < list.length) {
       if (list[i] == "*") {
-        list[i-1] = (int.parse(list[i-1]) * int.parse(list[i+1])).toString();
+        list[i-1] = (int.parse(list[i-1]) * int.parse(list[i+1])).toStringAsFixed(2);
         list.removeAt(i+1);
         list.removeAt(i);
+        i = 0;
+        print(list);
       } else if (list[i] == "/"){
-        list[i-1] = (double.parse(list[i-1]) / double.parse(list[i+1])).toString();
+        list[i-1] = (double.parse(list[i-1]) / double.parse(list[i+1])).toStringAsFixed(2);
         list.removeAt(i+1);
         list.removeAt(i);
+        i = 0;
+        print(list);
       }
+      i++;
     }
   }
   while (list.contains("+") || list.contains("-")) {
-    for (var i = 0; i < list.length; i++) {
+    var i = 0;
+    while (i < list.length) {
       if (list[i] == "+") {
-        list[i-1] = (double.parse(list[i-1]) + double.parse(list[i+1])).toString();
+        list[i-1] = (double.parse(list[i-1]) + double.parse(list[i+1])).toStringAsFixed(2);
         list.removeAt(i+1);
         list.removeAt(i);
+        i = 0;
+        print(list);
       } else if (list[i] == "-"){
-        list[i-1] = (double.parse(list[i-1]) - double.parse(list[i+1])).toString();
+        list[i-1] = (double.parse(list[i-1]) - double.parse(list[i+1])).toStringAsFixed(2);
         list.removeAt(i+1);
         list.removeAt(i);
+        i = 0;
+        print(list);
       }
+      i++;
     }
   }
-  print(list[0]);
+  print(list[0]); // sonuc
 }
